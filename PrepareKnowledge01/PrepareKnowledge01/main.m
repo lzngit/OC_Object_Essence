084975b2a24157f92721d94b66753d3ab0c9fd06
lzn <Tiger.Li@superid.me>
2019-05-19 17:35:31 +0800

    OC底层实现_准备知识
    查看NSObject类,在cpp文件中对应的结构体

---
+++ PrepareKnowledge01/PrepareKnowledge01/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  PrepareKnowledge01
+//
+//  Created by lzn on 2019/5/19.
+//  Copyright © 2019 PEAR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        // 创建NSObject对象
+        NSObject *obj = [[NSObject alloc] init];
+        
+        //打印
+        NSLog(@"%@", obj);
+        NSLog(@"obj的地址:%p", obj);
+        
+    }
+    return 0;
+}
